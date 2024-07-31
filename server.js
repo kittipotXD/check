bca75e01b93e835b81f7c236617c9455a17148b9
kittipotXD <kittipot.tutor@gmail.com>
2024-07-31 20:14:19 +0700

    app.use(express.static(path.join(__dirname, 'public')));
    แก้ตรงนี้มา

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.static("uploads"))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/data', (req,res)=>{
     res.json(data)
